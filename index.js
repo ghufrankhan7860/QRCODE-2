cc9c2bd482044504812ed6abe2fbd5faa8ed0e23
Ghufran <tabishmahmood445@gmail.com>
2024-02-14 10:10:26 +0530

    CSS File added

--- index.js
+++ index.js
@@ -8,20 +8,18 @@ import { fileURLToPath } from "url";
 import morgan from "morgan";
 const __dirname=dirname(fileURLToPath(import.meta.url));
 
-
-
-
 const app=express();
 const port=3000;
 
-// Serve static files from the 'public' folder
+// Serve static files from the 'public' folder -> this makes the css and  js file to work
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(morgan("tiny"));
+app.use(morgan("combined"));
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/public/index.html");
+
 })
 
 app.post("/submit", (req, res)=>{
--- public/logic.js
+++ public/logic.js
@@ -1,6 +1,26 @@
-// $("#submitBtn").on("click", function(event){
-//     // event.preventDefault();
-//     console.log("Cicked");
-//     var newParagraph = $(`<img src="/URL.png" alt="image PNG">`);
-//     $("#submitBtn").append(newParagraph);
-// })
+// // Specify the directory and file name
+// const directoryPath = '/';
+// const fileName = 'URL.png';
+
+// // Combine directory path and file name
+// const filePath = directoryPath + fileName;
+
+// // Check if the file exists
+// fetch(filePath)
+//     .then(response => {
+//         if (response.ok) {
+//             // File exists, add image element to the HTML
+//             const imageContainer = document.getElementById('imageContainer');
+//             const imgElement = document.createElement('img');
+//             imgElement.src = filePath;
+//             imgElement.alt = 'URL';
+//             imageContainer.appendChild(imgElement);
+//         } else {
+//             // File doesn't exist, you can handle this case or do nothing
+//             console.log('File not found.');
+//         }
+//     })
+//     .catch(error => {
+//         // Handle errors, for example, network issues
+//         console.error('Error checking file:', error);
+//     });
